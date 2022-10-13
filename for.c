4b2589040ec79481731f00d22028c71e6d2b3217
阿杰 <2831154320@qq.com>
2022-10-13 04:06:37 +0000

    add for.c.
    Signed-off-by: 阿杰 <2831154320@qq.com>

---
+++ for.c
@@ -0,0 +1,16 @@
+#include<stdio.h>
+
+int main()
+{
+        int i=0;
+            
+                for(i=1;i < 11;i++)
+                    {
+                                printf("%d",i);
+                    }
+                        
+                          
+                              return 0;
+}
+                    }
+}
