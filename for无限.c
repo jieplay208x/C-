c27dfb2a42b7310cc837271c3ea48806e1668ffa
阿杰 <2831154320@qq.com>
2022-10-13 04:09:00 +0000

    add for无限.c.
    Signed-off-by: 阿杰 <2831154320@qq.com>

---
+++ for无限.c
@@ -0,0 +1,13 @@
+#include<stdio.h>
+
+int main()
+{
+        for( ; ; )
+            {
+                    
+                            printf("6\n");
+            }
+                return 0;
+}
+            }
+}
