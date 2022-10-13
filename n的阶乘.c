3b2b89d521fd6b7e386f042d660dd05cc6337c3f
阿杰 <2831154320@qq.com>
2022-10-13 08:08:26 +0000

    add n的阶乘.c.
    Signed-off-by: 阿杰 <2831154320@qq.com>

---
+++ n的阶乘.c
@@ -0,0 +1,18 @@
+#include<stdio.h>
+
+int main()
+{
+        int i=0;
+            int n=0;
+                int c=1;
+                    scanf("%d",&n);
+                        for(i=1;i<=n;i++)
+                            {
+                                        c*=i;
+                            }
+                                
+                                    printf("%d",c);
+                                        return 0;
+}
+                            }
+}
