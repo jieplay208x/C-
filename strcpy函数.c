bd54fa692f51365c1f1e8620b18950a438f48d3a
阿杰 <2831154320@qq.com>
2022-10-17 13:38:26 +0000

    add strcpy函数.c.
    Signed-off-by: 阿杰 <2831154320@qq.com>

---
+++ strcpy函数.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+#include <string.h>
+int main()
+{
+        char arr1[20]={0} ;
+            char arr2[20]="abc";
+                strcpy(arr1,arr2);
+                    printf("%s",arr1);
+                        return 0;
+}
+
+}
