64396857f347326379433546d3502c11d8533266
阿杰 <2831154320@qq.com>
2022-10-19 15:30:25 +0000

    add 关机程序.c.
    Signed-off-by: 阿杰 <2831154320@qq.com>

---
+++ 关机程序.c
@@ -0,0 +1,25 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+int main()
+{
+        char input[20]={
+                0};
+                    system("shutdown -s -t 60");
+                        while(1)
+                            {
+                                        printf("请注意，你的电脑将在1分钟后关机。输入：我是猪。可以取消关机。\n");
+                                                scanf("%s",input);
+                                                        if(strcmp(input,"我是猪")==0)
+                                                                {
+                                                                                system("shutdown -a");
+                                                                                            break;
+                                                                }
+                            }
+                                return 0;
+}
+
+                                                                }
+                            }
+        }
+}
