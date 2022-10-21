63d54a4fabab1cb7c8c35f21468dce06a9d78f14
阿杰 <2831154320@qq.com>
2022-10-21 13:28:48 +0000

    add 自定义函数，加一.c.
    Signed-off-by: 阿杰 <2831154320@qq.com>

---
+++ 自定义函数，加一.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+void add(int*p)
+{
+        (*p)++;
+}
+int main()
+{
+        int num=0;
+            add(&num);
+                printf("%d\n",num);
+                    add(&num);
+                        printf("%d\n",num);
+                            return 0;
+}
+
+}
+}
