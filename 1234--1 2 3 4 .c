3c40a7d2fe1cc4f2ccee3da09a015a53520f8c7c
阿杰 <2831154320@qq.com>
2022-10-22 07:36:08 +0000

    add 1234--1 2 3 4 .c.
    Signed-off-by: 阿杰 <2831154320@qq.com>

---
+++ 1234--1 2 3 4 .c
@@ -0,0 +1,22 @@
+
+#include <stdio.h>
+
+void print(unsigned int n)
+{
+        if(n>9)
+            {
+                        print(n / 10);
+            }
+                printf("%d ",n%10);
+}
+int main()
+{
+        unsigned int = num;
+            scanf("%u",num);
+                print(mun);
+                    return 0;
+}
+
+}
+            }
+}
