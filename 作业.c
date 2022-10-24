dd303dab5a739f7abea1aa846836064a310791a4
阿杰 <2831154320@qq.com>
2022-10-24 15:28:03 +0000

    add 作业.c.
    Signed-off-by: 阿杰 <2831154320@qq.com>

---
+++ 作业.c
@@ -0,0 +1,24 @@
+#include <stdio.h>
+int main()
+{
+        int i=0;
+            int n=0;
+                for(i=1;i<100;i++)
+                    {
+                                if(i%10==9)
+                                        {
+                                                        n++;
+                                        }
+                                                if(i>=90)
+                                                        {
+                                                                        n++;
+                                                        }
+                    }
+                        printf("%d",n);
+                            return 0;
+}
+
+                                                        }
+                                        }
+                    }
+}
