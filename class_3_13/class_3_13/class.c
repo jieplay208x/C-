34b928c4ce57c5fad568db2b936d667b1f452d58
jieplay <2831154320@qq.com>
2023-03-13 21:59:25 +0800

    学习记录

---
+++ class_3_13/class_3_13/class.c
@@ -0,0 +1,17 @@
+#define _CRT_SECURE_NO_WARNINGS 1
+#include<stdio.h>
+#define ADD(x,y) ((x)+(y))
+int main()
+{
+	/*int a = 0;
+	int b = 0;
+
+	scanf("%d%d", &a, &b);
+	int num = a + b;
+	printf("%d \n", num);*/
+
+	printf("%d\n", 4 * ADD(2, 3));//ËüÊÇÌæ»»
+
+
+	return 0;
+}
