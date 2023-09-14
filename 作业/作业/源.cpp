117a6c340ce6701a814947a775607e1227ac94b0
jieplay <2831154320@qq.com>
2023-09-14 21:55:53 +0800

    现实作业的一部分

---
+++ 作业/作业/源.cpp
@@ -0,0 +1,33 @@
+#define _CRT_SECURE_NO_WARNINGS 1
+#include<iostream>
+using namespace std;
+//int ntr(int x)
+//{
+//	int y = 0;
+//	x = y = 5;
+//	return x;
+//}
+int main()
+{
+//	int a = 1, b = 2, c = 3, d = 4, m = 2, n = 2;
+//	cout << /*(m = a > b) && (n = c > d) <<*/ n << endl;
+	//int c = 'a';
+	//cout << 3.5 * 3 + 2 * 7 - 'a' <<endl;
+	//cout << 26/3+34%3+2.5 <<endl;
+
+	/*int a = 3, b=0, c=0;
+	a = b = (c = a += 6);
+	cout << a << b << c << endl;*/
+	//int a = 3; int b = 0;
+	//
+
+	//cout << (int)(a + 6.5) % 2 + ntr(a) << endl;
+	int a = 5, b = 4, c = 0;
+	int w = (a > b) && b && (b != c);
+	cout << w << endl;
+
+
+	system("pause");
+
+	return 0;
+}
